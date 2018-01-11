d88590b7a17a741339ea035b4026713621b943fe
louis <louis_boyle@hotmail.com>
2018-01-11 00:36:01 +0000

    ShowsOne is better, allowed for getting different dates on homepage

--- src/components/Home.js
+++ src/components/Home.js
@@ -7,16 +7,25 @@ class Home extends React.Component {
   constructor(props) {
     super(props);
 
+    this.onChange = this.onChange.bind(this);
+    this.searchCustomDate = this.searchCustomDate.bind(this);
+    this.getTodaySchedule = this.getTodaySchedule.bind(this);
+    this.openFilters = this.openFilters.bind(this);
     this.state = {
+      date: this.makeDate(),
       data: [],
       errors: {}
     };
   }
+  makeDate() {
+    const d = new Date();
+    return `${d.getFullYear()}-${d.getMonth()+1 > 9 ? d.getMonth()+1 : '0'+(d.getMonth()+1).toString()}-${d.getDate()}`;
+  }
   componentWillMount() {
     this.getTodaySchedule();
   }
-  getTodaySchedule() {
-    axios.get('http://api.tvmaze.com/schedule?country=US&date=2018-01-10')
+  getTodaySchedule(date = this.state.date) {
+    axios.get(`http://api.tvmaze.com/schedule?country=US&date=${date}`)
       .then(res => {
         return res.data.map(item => <ListItem item={ item } key={ item.id } />);
       })
@@ -25,10 +34,32 @@ class Home extends React.Component {
         this.setState({ errors: err }, () => console.log(this.state));
       });
   }
+  searchCustomDate(e) {
+    e.preventDefault();
+    this.getTodaySchedule();
+  }
+  onChange(e) {
+    this.setState({ [e.target.name]: e.target.value });
+  }
+  openFilters() {
+
+  }
   render() {
     return (
       <div className="Home">
-        <h3>Todays Schedule</h3>
+        <h3>Schedule</h3>
+        <form className="row" onSubmit={ this.searchCustomDate }>
+          <div className="u-full-width">
+            <input className="four columns" name="date" type="text" placeholder="yyyy-mm-dd" value={this.state.date} onChange={this.onChange} style={{marginRight: '1em'}} />
+          </div>
+          <div>
+            <input className="two columns" type="submit" value="Search" style={{marginRight: '1em'}} />
+          </div>
+          <div>
+            <button className="two columns" onClick={this.openFilters}>Filters</button>
+          </div>
+        </form>
+        <div id="filters-box"></div>
         <div id="res-list">{ this.state.data }</div>
       </div>
     );
--- src/components/ShowsOne.js
+++ src/components/ShowsOne.js
@@ -13,64 +13,69 @@ class ShowsOne extends React.Component {
   }
   componentWillMount() {
     this.getShow();
+    this.getCast();
   }
   getShow() {
     axios.get(`http://api.tvmaze.com${this.state.showUrl}`)
-      .then(res => this.setState({ show: res.data }, () => console.log(this.state)))
+      .then(res => this.setState({ show: res.data }))
       .catch(err => {
         this.setState({ errors: err }, () => console.log(this.state));
       });
   }
   getCast() {
-    http://api.tvmaze.com/shows/1/crew
-    axios.get(`http://api.tvmaze.com${this.state.showUrl}`)
-      .then(res => this.setState({ cast: res.data }, () => console.log(this.state)))
+    axios.get(`http://api.tvmaze.com${this.state.showUrl}/crew`)
+      .then(res => this.setState({ cast: res.data }))
       .catch(err => {
         this.setState({ errors: err }, () => console.log(this.state));
       });
   }
   render() {
-    const showDays = this.state.data.schedule ?
-      this.state.data.schedule.days.map(day => <li key={ day }>{ day }</li>) :
+    const show = this.state.show;
+    const showDays = show.schedule ?
+      show.schedule.days.map(day => <li key={ day }>{ day }</li>) :
       null;
+    const cast = this.state.cast.length ?
+      this.state.cast.map(p => <p key={ `${p.person.id}-${p.type.split(' ')[0]}` }>{ `${p.type}: ` }<a href={ p.person.url }>{ p.person.name }</a></p>) :
+      null;
+
     return (
       <div className="container">
 
         <div className="row">
           <div className="three columns">{
-            this.state.show.image ?
-              <img src={this.state.show.image.medium} alt={this.state.show.name}></img> :
+            show.image ?
+              <img src={show.image.medium} alt={show.name}></img> :
               <p>No image provided</p>
           }</div>
           <div className="nine columns">
-            <h3>{ this.state.show.name }</h3>
-            <div dangerouslySetInnerHTML={{ __html: this.state.show.summary }}></div>
+            <h3>{ show.name }</h3>
+            <div dangerouslySetInnerHTML={{ __html: show.summary }}></div>
           </div>
         </div>
 
         <div className="row">
-<<<<<<< HEAD
-          <div className="three columns">{
-            this.state.show.image ?
-              <img src={this.state.show.image.medium} alt={this.state.show.name}></img> :
-              <p>No image provided</p>
-          }</div>
-          <div className="nine columns">
-            <h3>{ this.state.show.name }</h3>
-            <div dangerouslySetInnerHTML={{ __html: this.state.show.summary }}></div>
-=======
-          <div className="three columns">
+          <div className="four columns">
             {
-              this.state.data.network ?
-                <p>{ this.state.data.network.name } : { this.state.data.network.country.timezone }</p> :
+              show.network ?
+                <p>{ show.network.name } : { show.network.country.timezone }</p> :
                 null
             }
-            <p>Shows at { this.state.data.schedule ? this.state.data.schedule.time : null } on:</p>
+            <p>Shows at { show.schedule ? show.schedule.time : null } on:</p>
             <ul>{ showDays }</ul>
           </div>
-          <div className="nine columns">
-
->>>>>>> f7e1535e55ac7c3dff21ad3a00cc2eba1fa589e9
+          <div className="four columns">
+            { cast }
+          </div>
+          <div className="four columns">
+            {
+              show.schedule ?
+                [
+                  <p key="0">Type: { show.type }</p>,
+                  <p key="1">Language: { show.language }</p>,
+                  <p key="2">Official site: <a href={ show.officialSite }>{ show.officialSite }</a></p>
+                ] :
+                null
+            }
           </div>
         </div>
 
